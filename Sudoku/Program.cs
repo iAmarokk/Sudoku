0f76fc7200f4e486a6d1f5bda9e88f769c65b764
Александр Небайкин <iamarokk@gmail.com>
2019-12-04 23:03:20 +0300

    Add project files.

---
+++ Sudoku/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace Sudoku
+{
+    class Program
+    {
+        Sudoku sudoku;
+        int difficulty = 30;
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Start();
+            Console.ReadKey();
+            //Console.WriteLine("Hello World!");
+        }
+
+        public void Start()
+        {
+            PrintFrame();
+            sudoku = new Sudoku(PrintDigit, SaveAnswer);
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Gray;
+                GenerateRandom(difficulty);
+                //LoadSudoku("sudoku.txt");
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            while (!sudoku.Solve());
+            //PrintDigit(1, 2, 3);
+        }
+
+        private void GenerateRandom(int count)
+        {
+            Random random = new Random();
+            if (count == 0) return;
+            if (count > Sudoku.max * Sudoku.max) return;
+            sudoku.ClearMap();
+
+            for (int c = 0; c < count; c++)
+            {
+                int x, y, d;
+                int loop = 777;
+                do
+                {
+                    x = random.Next(0, Sudoku.max);
+                    y = random.Next(0, Sudoku.max);
+                    d = random.Next(1, Sudoku.max + 1);
+                }
+                while
+                (--loop > 0 && !sudoku.PlaceDigit(x, y, d));
+            }
+            Console.SetCursorPosition(20, 5);
+            Console.WriteLine("Puzzle ready");
+        }
+
+        public void PrintFrame()
+        {
+            string symbol = " ";
+            for (int px = 0; px <= (Sudoku.sqr + 1) * Sudoku.sqr; px++)
+                for (int py = 0; py <= (Sudoku.sqr + 1) * Sudoku.sqr; py++)
+                {
+                    if (px % (Sudoku.sqr + 1) == 0 &&
+                        py % (Sudoku.sqr + 1) == 0)
+                        symbol = "+";
+                    else
+                        if (px % (Sudoku.sqr + 1) == 0)
+                        symbol = "|";
+                    else
+                        if (py % (Sudoku.sqr + 1) == 0)
+                        symbol = "-";
+                    else
+                        symbol = " ";
+                    Console.SetCursorPosition(px, py);
+                    Console.Write(symbol);
+                }
+
+        }
+
+        public void PrintDigit(int x, int y, int d)
+        {
+            int px = 1 + x + x / Sudoku.sqr;
+            int py = 1 + y + y / Sudoku.sqr;
+            Console.SetCursorPosition(px, py);
+            Console.Write(d == 0 ? " " : d.ToString());
+            //Thread.Sleep(10);
+        }
+
+        public void LoadSudoku(string filename)
+        {
+            string[] lines = File.ReadAllLines(filename);
+            int j = 0;
+            while (j < lines.Length)
+            {
+                if (lines[j].Contains("Поиск слова"))
+                    break;
+                j++;
+            }
+            j = j + 3;
+
+
+            for (int y = 0; y < Sudoku.max; y++)
+                for (int x = 0; x < Sudoku.max; x++)
+                {
+                    if (lines[j] != " ")
+                        sudoku.PlaceDigit(x, y, Convert.ToInt32(lines[j]));
+                    j++;
+                }
+        }
+
+        public void SaveAnswer()
+        {
+            using (StreamWriter file = new StreamWriter("solver.txt"))
+            {
+                string symbol = " ";
+                for (int py = 0; py <= (Sudoku.sqr + 1) * Sudoku.sqr; py++)
+                {
+                    for (int px = 0; px <= (Sudoku.sqr + 1) * Sudoku.sqr; px++)
+
+                    {
+                        if (px % (Sudoku.sqr + 1) == 0 &&
+                            py % (Sudoku.sqr + 1) == 0)
+                            symbol = "+";
+                        else
+                            if (px % (Sudoku.sqr + 1) == 0)
+                            symbol = "|";
+                        else
+                            if (py % (Sudoku.sqr + 1) == 0)
+                            symbol = "-";
+                        else
+                        {
+                            int x = px - 1 - px / (Sudoku.sqr + 1);
+                            int y = py - 1 - py / (Sudoku.sqr + 1);
+                            symbol = sudoku.map[x, y].ToString();
+                        }
+                        file.Write(symbol);
+                    }
+                    file.WriteLine();
+                }
+                Console.SetCursorPosition(20, 7);
+                Console.WriteLine("Solved");
+                //Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Sudoku/Sudoku.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sudoku
+{
+    delegate void dePrintDigit(int x, int y, int d);
+    delegate void deSaveAnswer();
+
+    class Sudoku
+    {
+        public const int max = 9; // map size
+        public const int sqr = 3;
+
+        bool found;
+        int count=0;
+
+        public int [,] map { get; private set; }
+
+        private dePrintDigit PrintDigit;
+        private deSaveAnswer SaveAnswer;
+
+        public Sudoku(dePrintDigit printDigit, deSaveAnswer saveAnswer)
+        {
+            map = new int[max, max];
+            PrintDigit = printDigit;
+            SaveAnswer = saveAnswer;
+            ClearMap();
+        }
+
+        public void ClearMap()
+        {
+            for (int x = 0; x < max; x++)
+                for(int y = 0; y < max; y ++)
+                {
+                    ClearDigit(x,y);
+                }
+        }
+
+        public bool PlaceDigit(int x, int y, int d)
+        {
+            if (x < 0 || x >= Sudoku.max) return false;
+            if (y < 0 || y >= Sudoku.max) return false;
+            if (d <= 0 || d > Sudoku.max) return false;
+
+            if (map[x, y] == d) return true;
+            if (map[x, y] != 0) return false;
+
+            for (int cx = 0; cx < Sudoku.max; cx++)
+                if (map[cx, y] == d)
+                    return false;
+
+            for (int cy = 0; cy < Sudoku.max; cy++)
+                if (map[x, cy] == d)
+                    return false;
+
+            int sx = Sudoku.sqr * (x / Sudoku.sqr);
+            int sy = Sudoku.sqr * (y / Sudoku.sqr);
+
+            for (int cx = sx; cx < sx + Sudoku.sqr; cx++)
+                for (int cy = sy; cy < sy + Sudoku.sqr; cy++)
+                    if (map[cx, cy] == d)
+                        return false;
+            map[x, y] = d;
+            PrintDigit(x, y, d);
+            return true;
+        }
+
+        public bool Solve()
+        {
+            found = false;
+            NextDigit(0);
+            return found;
+        }
+
+        public void NextDigit(int step)
+        {
+            count++;
+            if (found) return;
+            if(step == max*max)
+            {
+                found = true;
+                SaveAnswer();
+                return;
+            }
+
+            int x = step % max;
+            int y = step / max;
+
+            if(map[x,y] > 0)
+            {
+                NextDigit(step + 1);
+                return;
+            }
+
+            for(int d = 1; d <= max; d++)
+            {
+                if(PlaceDigit(x,y,d))
+                {
+                    NextDigit(step + 1);
+                    ClearDigit(x,y);
+                }
+            }
+        }
+
+        private void ClearDigit(int x, int y)
+        {
+            if (x < 0 || x >= Sudoku.max) return;
+            if (y < 0 || y >= Sudoku.max) return;
+
+            if (map[x, y] == 0) return;
+
+            map[x, y] = 0;
+            PrintDigit(x, y, 0);
+        }
+
+        //private void SaveAnswer()
+        //{
+        //    Console.WriteLine();
+        //    Console.WriteLine();
+        //    Console.WriteLine("Количество итераций:" + " " +count);
+        //    Console.ReadKey();
+        //}
+
+    }
+}
